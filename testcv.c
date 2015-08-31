07a3d5ef333fc47ed2b452f98059539022c84cfb
Jaycee <zhjcyx@gmail.com>
2015-08-31 23:42:37 +0800

    add

---
+++ testcv.c
@@ -0,0 +1,19 @@
+#include <stdio.h
+#include<opencv2/opencv.hpp
+
+using namespace cv;
+
+int main(int argc, char *argv[
+{
+    Mat src;
+        //Init
+            printf("load test.jpg and L=15 automatically\n";
+                src=imread("/home/jaycee/Desktop/test.jpg";
+                    imshow("src",src;
+                        waitKey(0;
+                            //process
+                                Mat dst(src,Rect(1,1,src.rows/3, src.cols/3;
+                                    //imshow("dst",dst;
+                                        //waitKey(0;
+                                        
+                                        })))))))))]>>
